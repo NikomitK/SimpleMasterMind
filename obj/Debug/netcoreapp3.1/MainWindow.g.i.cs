884e6e423c9c7d4d1a1a779e69e510ca71371203
NikomitK <68833349+NikomitK@users.noreply.github.com>
2020-09-09 02:07:19 +0200

    Uploading the pre written files

--- obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -149,7 +149,7 @@ namespace Mastermindgui {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Mastermindgui;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Mastermindgui;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
